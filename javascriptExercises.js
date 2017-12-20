57fdc4a9c9fe39c545c858967ccdfd8cc2e2d29a
BrianSheridan <briansheridan732@gmail.com>
2017-12-20 22:13:33 +0000

    Intial Commit

---
+++ javascriptExercises.js
@@ -0,0 +1,204 @@
+ <script>
+    //var number1 = 35;
+   // var number2 = 40
+    //alert('My favourite Number is' + number2);
+
+    //var test = 'This is a test'
+    //var Test = 'This is another test'
+
+    //alert(Test);
+
+    // CAMEL CASE
+    //var myFavouriteNumber
+
+    // partial case
+    //var myFavouriteNumber;
+
+    // underscore
+    //var my_favourite_number;
+
+
+    // ARRAY EXAMPLES
+    //var colors =['red', 'blue', 'green'];
+
+    //colors.push('black');
+
+    //alert(colors[0]);
+
+    //var numbers = [5,77,6,43,'Seven',1,55];
+
+    //alert(numbers[0] + numbers[2]);
+
+    //alert(numbers.sort());
+
+
+    //LOOPS EXAMPLES
+    //for(var 1 = 0; x < 10; i++{
+    //     console.log('This is my Number' + i);
+    //}
+
+    //WHILE LOOPS
+    //var i = 0;
+    //while(i < 10){
+    //  console.log(i);
+    //  i++;
+    //}
+
+    // FOR EACH LOOP
+    //var numbers = [33,54,76,34,2,6];
+
+    //numbers.forEach(function(number){
+     //console.log(number);
+    //});
+
+    //var numbers = [33,54,76,34,2,6];
+    //numbers.reverse();
+
+    //for(var i = 0; i < numbers.length; i++){
+    //  console.log(numbers[i]);
+    //}
+
+    // CONDITIONALS
+    //if(1 == 1){
+    //  console.log('This is True');
+    //}
+
+    //var var1 = 3;
+    //var var2 = 4;
+    //if(var1 != var2){
+    //  console.log('This is True');
+    //}
+
+    //var var1 = 85;
+    //var var2 = 73;
+    //if (var1 == var2) {
+    //  console.log('This is true');
+    //} else{
+    //  console.log('This is false, Fuck the gaa');
+    
+
+    // var var1 = 3;
+    // var var2 = 3;
+    // if (var1 == var2 && var1 == 2) {
+    //  console.log('This is true');
+    // }    else{
+ //          console.log('This is false');
+    //  }
+
+    // var var1 = 3;
+    // var var2 = 3;
+    // if (var1 == var2 || var1 == 2) {
+    //  console.log('This is true');
+    // }    else{
+    //  console.log('This is false');
+    // }
+
+    //SWITCH STATEMENTS
+
+    // var fruit = 'grapes';
+
+    // switch(fruit){
+    //     case "banana":
+    //     alert('I like Bananas');
+    //     break;
+    //     case "apples":
+    //     alert('I like Oranges');
+    //     break;
+    //     default:
+    //     alert('I love the Craic')
+    //     break;
+    // }
+
+    //OBJECT LITERAL
+
+    // var person = {
+    //     firstName: 'Brian',
+    //     lastName: 'Sheridan',
+    //     age: 30,
+    //     children: ['drums', 'Guitar']
+    // }
+
+    //     console.log(person.children[1]);
+
+    // var person = {
+    //     firstName: 'Brian',
+    //     lastName: 'Sheridan',
+    //     age: 30,
+    //     children: ['drums', 'Guitar'],
+    //     address: {
+    //         street: 'Courtown Road',
+    //         town: 'Kilcock',
+    //         county: 'Kildare',
+    //     },
+    // }
+
+    //     console.log(person.address.town);
+
+    // var person = {
+    //     firstName: 'Brian',
+    //     lastName: 'Sheridan',
+    //     age: 30,
+    //     children: ['drums', 'Guitar'],
+    //     address: {
+    //         street: 'Courtown Road',
+    //         town: 'Kilcock',
+    //         county: 'Kildare',
+    //     },
+    //     fullName: function(){
+    //         return this.firstName +" "+ this.lastName;
+    //     }
+    // }
+
+    // console.log(person.fullName());
+
+    // OBJECT CONSTRUCTOR
+    // var apple = new Object();
+    // apple.color = 'red';
+    // apple.shape = 'round';
+
+    //  apple.describe = function(){
+    //      return 'An Apple is the color '+this.color+' and is the shape '
+    //        +this.shape;
+         
+    //  }
+
+    //  console.log(apple.describe());
+
+    //CONSTRUCTOR PATTERN
+
+    // function Fruit(name, color, shape){
+    //     this.name = name;
+    //     this.color = color;
+    //     this.shape = shape;
+
+    //     this.describe = function(){
+    //         return 'A '+this.name+' is the color '+this.color+
+    //         ' and is the shape '+this.shape;
+    //     }
+    // }
+
+    //  var apple = new Fruit('apple', 'red', 'Round');
+    //  var Melon = new Fruit('Melon', 'Green', 'Round');
+
+    // console.log(apple.describe());
+    
+
+    //OBJECTS
+    // var users = [
+    //     {
+    //         name:'Brian Sheridan',
+    //         age: 30
+    //     },
+    //     {
+    //         name:'Tara Sheridan',
+    //         age: 22
+    //     },
+    //     {
+    //         name:'Joe Kelly',
+    //         age: 30
+    //     }
+    // ];
+
+    // console.log(users[2].age);
+
+</script>
